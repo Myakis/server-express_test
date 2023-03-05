fa5b9c1415053dbbb78896011f7def65bed15d68
Myakis <myakishevandrey@gmail.com>
2023-03-05 14:35:39 +0300

    Вывод статистики по тренировкам у пользователя

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -4,6 +4,29 @@ import { UserField } from '../../utils/user.js'
 
 export const getProfile = asyncHandler(async (req, res) => {
   const user = await prisma.user.findUnique({ where: { id: req.user.id }, select: UserField })
-
-  res.json({ user })
+  const countExerciseCompleted = await prisma.exerciseLog.count({
+    where: { userId: req.user.id, isCompleted: true },
+  })
+  const kgs = await prisma.exerciseTime.aggregate({
+    where: {
+      exerciseLog: {
+        userId: +req.user.id,
+      },
+      isCompleted: true,
+    },
+    _sum: {
+      weight: true,
+    },
+  })
+  const countWorkout = await prisma.workoutLog.count({
+    where: { userId: req.user.id, isCompleted: true },
+  })
+  res.json({
+    ...user,
+    statistics: [
+      { label: 'Минуты', value: Math.ceil(countExerciseCompleted * 2.3) ||0},
+      { label: 'Килограммы', value: kgs._sum.weight || 0 },
+      { label: 'Тренировки', value: countWorkout || 0 },
+    ],
+  })
 })
